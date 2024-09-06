4ce25ec6688603c31c3b0d5d697f655a6ea41536
BoraEfe <Bora.e@icloud.com>
2024-09-06 12:00:37 +0200

    Bijwerking van Header en Footer

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import './Footer.css';
+
+const Footer = () => {
+    return (
+        <footer>
+        <p>© 2021 GeoProfs</p>
+        </footer>
+    );
+};
+
+export default Footer;
---
+++ src/Pages/HomePage/HomePage.jsx
@@ -0,0 +1,13 @@
+import './HomePage.css';
+
+const HomePage = () => {
+    return (
+        <div>
+            <h2>Welcome to GeoProfs!</h2>
+            <p>GeoProfs is a website that helps you learn about the world's countries.</p>
+            <p>Click on a country's name to learn more about it.</p>
+        </div>
+    );
+};
+
+export default HomePage;
