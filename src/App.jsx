71a864280213c2e261b0ddae431fe87b06e5c765
DaanRVRS <dfpreuvers@gmail.com>
2024-09-16 11:46:29 +0200

    gp-1 Header component aangemaakt

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,16 @@
 import { useState } from 'react'
 import Header from './Components/Header/Header'
-import Footer from './Components/Footer/Footer'
 import HomePage from './Pages/HomePage/HomePage'
 import './App.css'
 import { Routes, Route} from 'react-router-dom'
 
 const App = () => {
-
-    return (
+  return (
     <body>
       <Header />
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-        </Routes>
-      <Footer />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
     </body>
   )
 }
