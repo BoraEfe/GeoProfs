63f2762cb0861063b2c5b5311b10e0723d08fbe0
Bora <114698111+borbor024@users.noreply.github.com>
2024-09-21 19:32:37 +0200

    fix datum2

--- src/Components/date.jsx
+++ src/Components/date.jsx
@@ -1,18 +1,28 @@
 import React from 'react';
+import {useState, useEffect} from 'react';
 
 const CurrentDate = () => {
-    const today = new Date();
-    const day = today.getDate();
-    const month = today.getMonth() + 1;
-    const year = today.getFullYear();
-    const minute = today.getMinutes();
-    const hour = today.getHours();
+    const [currentDate, setCurrentDate] = useState(''); 
 
-    const formattedMonth = month < 10 ? `0${month}` : month;
-    const formattedMinute = minute < 10 ? `0${minute}` : minute;
-
-    const currentDate = `${day}/${formattedMonth}/${year} ${hour}:${formattedMinute}`;
+    const updateDate = () => {
+        const today = new Date();
+        const day = today.getDate();
+        const month = today.getMonth() + 1;
+        const year = today.getFullYear();
+        const minute = today.getMinutes();
+        const hour = today.getHours();
     
+        const formattedMonth = month < 10 ? `0${month}` : month;
+        const formattedMinute = minute < 10 ? `0${minute}` : minute;
+    
+        setCurrentDate(`${day}/${formattedMonth}/${year} ${hour}:${formattedMinute}`);
+    }
+    useEffect(() =>
+    {
+        const interval = setInterval(updateDate, 1000)
+        return () => clearInterval(interval)
+    }, [])
+
     return(
         <li>
             <a>{currentDate}</a>
