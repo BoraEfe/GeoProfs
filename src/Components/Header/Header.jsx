7b236f1eca58b4c96034e03851603afa77738db2
BoraEfe <Bora.e@icloud.com>
2024-09-25 11:06:51 +0200

    fix datum2

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -24,18 +24,12 @@ const Header = () => {
             <li>
               <a href="/ziekmelden">Ziekmelden</a>
             </li>
-<<<<<<< HEAD
+            <Date />
             { localStorage.getItem('isLoggedIn') ? (
               <li className="logout-button" onClick={() => {handleLogout()}}>
                 <a><FontAwesomeIcon icon={faArrowRightFromBracket} /> Uitloggen</a>
               </li>
             ) : (<div></div>)}
-=======
-            <Date/>
-            <li className="logout-button" onClick={() => {handleLogout()}}>
-              <a><FontAwesomeIcon icon={faArrowRightFromBracket} /> Uitloggen</a>
-            </li>
->>>>>>> 72de5e2f108e021cc74d1754dbe50ac9aaaebb35
           </ul>
         </nav>
         { localStorage.getItem('isLoggedIn') ? (
