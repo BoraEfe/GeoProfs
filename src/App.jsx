04cbee9742a2a9c48ab8ded5a6d197283830a2fd
kingside50 <120608937+kingside50@users.noreply.github.com>
2024-09-25 10:22:23 +0200

    vakantie pagina

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,11 @@ import { useState } from 'react'
 import Header from './Components/Header/Header'
 import HomePage from './Pages/HomePage/HomePage'
 import Login from './Pages/Login/Login'
+import VakantieDagen from './Pages/Vakantiedagen/Vakantiedagen'
 import './App.css'
 import { Routes, Route} from 'react-router-dom'
 import Ziekmelden from './Pages/Ziekmelden/Ziekmelden'
+
 
 
 const App = () => {
@@ -15,6 +17,7 @@ const App = () => {
           <Route path="/" element={<HomePage />} />
           <Route path="/Login" element={<Login />} />
           <Route path="/ziekmelden" element={<Ziekmelden />} />
+          <Route path="/vakantiedagen" element={<VakantieDagen />} />
         </Routes>
     </body>
   )
---
+++ src/Pages/Vakantiedagen/Vakantiedagen.jsx
@@ -0,0 +1,69 @@
+import '../HomePage/HomePage.css'
+import Agenda from '../../Components/Agenda/Agenda';
+import { useEffect, useState } from 'react';
+import { collection, getDocs, where, query } from 'firebase/firestore';
+import { db } from '../../firebase';
+import cancelPageSwitchWhenNotLoggedIn from '../../Components/cancelPageSwitchWhenNotLoggedIn';
+
+    const vakantieDagen = () => {
+        cancelPageSwitchWhenNotLoggedIn();
+
+        const [vakantieDagen, setVakantieDagen] = useState();
+        const email = localStorage.getItem('email');
+
+        useEffect(() => {
+            const fetchVakantieDagen = async () => {
+                try {
+                    const usersRef = collection(db, 'users');
+                    const q = query(usersRef, where('email', '==', email));
+                    const querySnapshot = await getDocs(q);
+    
+                    if (!querySnapshot.empty) {
+                        const userDoc = querySnapshot.docs[0].data(); 
+                        setVakantieDagen(userDoc.vakantiedagen);
+                    } else {
+                        console.error('Gebruiker niet gevonden');
+                    }
+                } catch (error) {
+                    console.error('Error bij het ophalen van vakantiedagen: ', error);
+                }
+            };
+    
+            fetchVakantieDagen();
+        }, [email]);
+
+        return (
+            <main>
+                <div className="homepage-container-1">
+                    <button onClick={() => {navigate('/ziekmelden')}}>
+                        Ziekmelden
+                    </button>
+                    <button onClick={() => {navigate('/vakantie-dagen')}}>
+                        Vakantiedagen opnemen
+                    </button>
+                    <button onClick={() => {navigate('/verlof')}}>
+                        Verlof aanvragen
+                    </button>
+                    <div className='vakantiedagen'>
+                        {vakantieDagen !== null ? (
+                             `Aantal vakantiedagen: ${vakantieDagen}`
+                        ) : (
+                            'Vakantiedagen laden...'
+                        )}
+
+                    </div>
+                </div>
+                <div className='homepage-container-2'>
+                    <h1 className='welkom-title'>
+                        Welkommm, {localStorage.getItem('firstname')} {localStorage.getItem('lastname')}
+                    </h1>
+                </div>
+                <div className='homepage-container-3'>
+                    <Agenda/>
+                </div>
+            </main>
+        );
+    };
+
+    export default vakantieDagen;
+    
