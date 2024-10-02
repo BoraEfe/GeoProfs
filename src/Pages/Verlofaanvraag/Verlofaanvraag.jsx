2bd3f7bf3dc3c48c563a8c17d1a1c2a511f71f86
Jayr0 <122980334+Jayr0@users.noreply.github.com>
2024-10-02 10:41:55 +0200

    ziekmelden pagina

--- src/Pages/Verlofaanvraag/Verlofaanvraag.jsx
+++ src/Pages/Verlofaanvraag/Verlofaanvraag.jsx
@@ -1,42 +1,55 @@
 import './Verlofaanvraag.css';
 import React, { useState } from 'react';
+import { collection, addDoc } from 'firebase/firestore';
+import { db } from '../../firebase';
+
 const Verlofaanvraag = () => {
     const [verlofBeginData, setVerlofBeginData] = useState ('');
     const [verlofEindData, setVerlofEindData] = useState ('');
-
+    const [reden, setReden] = useState('');
+    const [confirmation, setConfirmation] = useState();
+    
     const test = (e) => {
         e.preventDefault();
         console.log('test');
     }
   return (
-    <> 
+    <>
       <div className='verlofaanvraag-container'>
         <div className='verlofaanvraag-form'>
             <form onSubmit={test}>
+              <div className="data">
                 <p>Van datum</p>
                 <input 
                     type='date' 
                     value={verlofBeginData}
+                    onChange={(e) => setVerlofBeginData(e.target.value)}
                     required>                        
                 </input>
+                <p>{verlofBeginData}</p>
                 <p>Tot datum</p>
                 <input 
                     type='date' 
                     value={verlofEindData}
+                    onChange={(e) => setVerlofEindData(e.target.value)}
                     required>
                 </input>
-                <p>Reden</p>
-                <textarea
-                placeholder='Reden van verlof'
-                style={{ height: '15vh' }}
-                maxLength={500} 
-                >
-                </textarea>
+                <p>{verlofEindData}</p>
                 <button 
                 className='submit-button' 
                 type='submit'>
                     Verstuur
                 </button>
+                </div>
+                <div className="reden">
+                <p>Reden van verlof</p>
+                <textarea
+                placeholder='...'
+                maxLength={500}
+                required
+                >
+                </textarea>
+                </div>
             </form>
         </div>
       </div>
