31aed2bcf7cac17f0f79728fea4686d14ba8bd7b
DaanRVRS <dfpreuvers@gmail.com>
2024-10-10 12:42:17 +0200

    merged with main

--- src/Pages/Vakantiedagen/Vakantiedagen.jsx
+++ src/Pages/Vakantiedagen/Vakantiedagen.jsx
@@ -2,8 +2,8 @@ import './Vakantiedagen.css';
 import React, { useState } from 'react';
 
 const Vakantiedagen = () => {
-    const [verkantieBeginData, setVerkantieBeginData] = useState ('');
-    const [verkantieEindData, setVerkantieEindData] = useState ('');
+    const [vakantieBeginData, setvakantieBeginData] = useState ('');
+    const [vakantieEindData, setvakantieEindData] = useState ('');
 
     const test = (e) => {
         e.preventDefault();
@@ -11,24 +11,24 @@ const Vakantiedagen = () => {
     }
     return(
         <> 
-        <div className='verkantieaanvraag-container'>
-          <div className='verkantieaanvraag-form'>
+        <div className='vakantieaanvraag-container'>
+          <div className='vakantieaanvraag-form'>
               <form onSubmit={test}>
                   <p>Van datum</p>
                   <input 
                       type='date' 
-                      value={verkantieBeginData}
+                      value={vakantieBeginData}
                       required>                        
                   </input>
                   <p>Tot datum</p>
                   <input 
                       type='date' 
-                      value={verkantieEindData}
+                      value={vakantieEindData}
                       required>
                   </input>
                   <p>Reden</p>
                   <textarea
-                  placeholder='Reden van verkantie'
+                  placeholder='Reden van vakantie'
                   style={{ height: '15vh' }}
                   maxLength={500}
                   >
