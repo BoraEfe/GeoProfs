eece04b3dabb66dccc328e8a364fbc1c743cca8f
DaanRVRS <dfpreuvers@gmail.com>
2024-11-21 15:31:58 +0100

    fix of error in checkForFirstLogin

--- src/Components/CheckForFirstLogin/CheckForFirstLogin.jsx
+++ src/Components/CheckForFirstLogin/CheckForFirstLogin.jsx
@@ -1,14 +1,12 @@
 const CheckForFirstLogin = (user) => {
-       
-        if (user.tijdelijkWachtwoord === "" || user.tijdelijkWachtwoord === null || user.tijdelijkWachtwoord === undefined){ 
-            console.log('niks aan de hand');
-            return;
-        } else {
-            console.log("Uw wachtwoord is nog niet veranderd. Verander uw wachtwoord nu!");
-            window.location.href = '/ChangeTemporaryPassword';
-            return;
-        }
-    };
-}
+    if (user.tijdelijkWachtwoord === "" || user.tijdelijkWachtwoord === null || user.tijdelijkWachtwoord === undefined){ 
+        console.log('niks aan de hand');
+        return;
+    } else {
+        console.log("Uw wachtwoord is nog niet veranderd. Verander uw wachtwoord nu!");
+        window.location.href = '/ChangeTemporaryPassword';
+        return;
+    }
+};
 
 export default CheckForFirstLogin;
