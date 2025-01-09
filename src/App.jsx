07252ab6f4169a3b2a5de33781f6a636a7feee2a
BoraEfe <Bora.e@icloud.com>
2025-01-09 13:12:25 +0100

    fixen van date en van verlof kiezen

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import HomePage from './pages/HomePage/HomePage'
 import Login from './pages/Login/Login'
 import './App.css'
 import { Routes, Route} from 'react-router-dom'
-import Ziekmelden from './Pages/Ziekmelden/Ziekmelden'
 import Verlofaanvraag from './Pages/Verlofaanvraag/Verlofaanvraag'
 import VakantieDagen from './Pages/Vakantiedagen/Vakantiedagen'
 import { UserProvider } from './functions/context/User'
@@ -27,7 +26,6 @@ const App = () => {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/Login" element={<Login />} />
-        <Route path="/ziekmelden" element={<Ziekmelden />} />
         <Route path="/verlofaanvraag" element={<Verlofaanvraag />} />
         <Route path="/vakantiedagen" element={<VakantieDagen />} />
         <Route path="/AddUser" element={<AddUser />} />
