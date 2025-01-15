da0be97a4b1937d24ba75da51175e4b0472b7d3f
Jayr0 <122980334+Jayr0@users.noreply.github.com>
2025-01-15 14:15:45 +0100

    komt

--- src/Components/verlofInfo/VerlofInfo.jsx
+++ src/Components/verlofInfo/VerlofInfo.jsx
@@ -33,77 +33,75 @@ const VerlofInfo = ({ aanvraag, aanvraagId, onClose }) => {
                 console.log('No such document');
             }
         } catch (error) {
-            console.error('Fout bij het verplaatsen van de verlofaanvraag: ', error);
+            console.error('Error moving leave request: ', error);
         }
     }
 
     if (!aanvraag) return null;
 
     return (
-        <div className={styles.overlay}>
-            <div className={styles.verlofDetails}>
-                <button onClick={onClose} className={styles.closeButton}>
-                    <FontAwesomeIcon icon={faX} />
-                </button>
-                <h2 className={styles.username}>
-                    details van <strong>{aanvraag.medewerker}</strong>
-                </h2>
-                <p>
-                    aangevraagd op: <strong>{new Date(aanvraag.timestamp.seconds * 1000).toLocaleDateString()}</strong>
-                </p>
-                <p>
-                    Van: <strong>{aanvraag.beginDatum}</strong>
-                </p>
-                <p>
-                    Tot: <strong>{aanvraag.eindDatum}</strong>
-                </p>
-                <p>
-                    status: <strong>{!aanvraag.isApproved ? 'openstaand' : 'goedgekeurd'}</strong>
-                </p>
-                <p>
-                    Reden: <strong>{aanvraag.reden}</strong>
-                </p>
+        <div className={styles.verlofDetails}>
+            <button onClick={onClose} className={styles.closeButton}>
+                <FontAwesomeIcon icon={faX} />
+            </button>
+            <h2 className={styles.username}>
+                details van <strong>{aanvraag.medewerker}</strong>
+            </h2>
+            <p>
+                aangevraagd op: <strong>{new Date(aanvraag.timestamp.seconds * 1000).toLocaleDateString()}</strong>
+            </p>
+            <p>
+                Van: <strong>{aanvraag.beginDatum}</strong>
+            </p>
+            <p>
+                Tot: <strong>{aanvraag.eindDatum}</strong>
+            </p>
+            <p>
+                status: <strong>{!aanvraag.isApproved ? 'openstaand' : 'goedgekeurd'}</strong>
+            </p>
+            <p>
+                Reden: <strong>{aanvraag.reden}</strong>
+            </p>
 
-                {role !== '4' && (
-                    <>
-                        <textarea
-                            placeholder="Opmerking"
-                            style={{ height: '15vh' }}
-                            maxLength={500}
-                            value={leaveNote}
-                            onChange={(e) => setLeaveNote(e.target.value)}
-                        />
-                        <div className={styles.buttons}>
-                            <button
-                                onClick={async () => {
-                                    try {
-                                        await moveLeaveRequest(aanvraag.uuid, 'goedgekeurdeAanvragen', true);
-                                        window.location.reload();
-                                    } catch (error) {
-                                        console.error('Fout bij goedkeuren van de verlofaanvraag: ', error);
-                                    }
-                                }}
-                                className={styles.approveButton}
-                            >
-                                Goedkeuren
-                            </button>
-                            <button
-                                onClick={async () => {
-                                    try {
-                                        await moveLeaveRequest(aanvraag.uuid, 'afgekeurdeAanvragen', false);
-                                        window.location.reload();
-                                    } catch (error) {
-                                        console.error('Fout bij afkeuren van de verlofaanvraag: ', error);
-                                    }
-                                }}
-                                className={styles.rejectButton}
-                            >
-                                Afkeuren
-                            </button>
-                        </div>
-                    </>
-                )}
-            </div>
+            {role !== '4' && (
+                <>
+                    <textarea
+                        placeholder="Opmerking"
+                        style={{ height: '15vh' }}
+                        maxLength={500}
+                        value={leaveNote}
+                        onChange={(e) => setLeaveNote(e.target.value)}
+                    />
+                    <div className={styles.buttons}>
+                        <button
+                            onClick={async () => {
+                                try {
+                                    await moveLeaveRequest(aanvraag.uuid, 'goedgekeurdeAanvragen', true);
+                                    window.location.reload();
+                                } catch (error) {
+                                    console.error('Error approving leave request: ', error);
+                                }
+                            }}
+                            className={styles.approveButton}
+                        >
+                            Goedkeuren
+                        </button>
+                        <button
+                            onClick={async () => {
+                                try {
+                                    await moveLeaveRequest(aanvraag.uuid, 'afgekeurdeAanvragen', false);
+                                    window.location.reload();
+                                } catch (error) {
+                                    console.error('Error rejecting leave request: ', error);
+                                }
+                            }}
+                            className={styles.rejectButton}
+                        >
+                            Afkeuren
+                        </button>
+                    </div>
+                </>
+            )}
         </div>
     );
 };
