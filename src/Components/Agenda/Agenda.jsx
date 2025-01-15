80914f0d52141a21c524b732420f8c04f3fb3e1d
Bora <bora.e@icloud.com>
2025-01-15 12:48:51 +0100

    ombouwen van agenda

--- src/Components/Agenda/Agenda.jsx
+++ src/Components/Agenda/Agenda.jsx
@@ -8,6 +8,10 @@ const Agenda = () => {
 
     const months = ['Januari', 'Februari', 'Maart', 'April', 'Mei', 'Juni', 'Juli', 'Augustus', 'September', 'Oktober', 'November', 'December'];
 
+    const fetchLeaves = async () => {
+        const querySnapShot = await getDocs 
+    }
+
     function calculateWeekDays(){
         let year = new Date().getFullYear();    
         let endOfMonth = new Date(year, month + 1,0).getDate();
@@ -27,7 +31,26 @@ const Agenda = () => {
 
     useEffect(() => {
         calculateWeekDays();
-    },[month]);
+    },[month, year]);
+
+    const previousMonth = () => {
+        if(month === 0){
+            setMonth(11);
+            setYear(year - 1);
+        }
+        else{
+            setMonth(month - 1);
+        }
+    }
+    const nextMonth = () => {
+        if(month === 11){
+            setMonth(0);
+            setYear(year + 1);
+        }
+        else{
+            setMonth(month + 1);    
+        }
+    }
 
     return(
         <div className='agenda-container'>
@@ -35,13 +58,13 @@ const Agenda = () => {
             <div className='buttons-container'>
                 <button 
                  className='choose-month'
-                 onClick = {() => setMonth((month + 11 ) % 12)}>
+                 onClick = {() => previousMonth()}>
                     <span>{'<'}</span>
                 </button>
                 <div className='current-month'>{months[month] + ` ` + year}</div>
                 <button
                  className='choose-month'
-                 onClick= {() => setMonth((month + 1) % 12)}>
+                 onClick= {() => nextMonth()}>
                     <span>{'>'}</span>
                 </button>
             </div>
